67afa5a763491786206f7c0eaaee16a3df315391
Yakup Şimşek <ysimsek@hacettepe.edu.tr>
2022-07-31 13:18:15 +0300

    Proje dosyası ekle.

---
+++ HastaneSistemi/Login.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace HastaneSistemi
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblhata = new System.Windows.Forms.Label();
+            this.btngiris = new System.Windows.Forms.Button();
+            this.pnlsifre = new System.Windows.Forms.Panel();
+            this.lblsifre = new System.Windows.Forms.Label();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pnlad = new System.Windows.Forms.Panel();
+            this.lblad = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnkapat = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.pnlsifre.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pnlad.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(79)))), ((int)(((byte)(107)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 550);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(193)))), ((int)(((byte)(201)))));
+            this.label3.Location = new System.Drawing.Point(42, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hastanemize";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(193)))), ((int)(((byte)(201)))));
+            this.label1.Location = new System.Drawing.Point(52, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hoş geldiniz";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblhata);
+            this.panel2.Controls.Add(this.btngiris);
+            this.panel2.Controls.Add(this.pnlsifre);
+            this.panel2.Controls.Add(this.pnlad);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnkapat);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(300, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 550);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblhata
+            // 
+            this.lblhata.AutoSize = true;
+            this.lblhata.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblhata.ForeColor = System.Drawing.Color.Red;
+            this.lblhata.Location = new System.Drawing.Point(20, 96);
+            this.lblhata.Name = "lblhata";
+            this.lblhata.Size = new System.Drawing.Size(0, 21);
+            this.lblhata.TabIndex = 7;
+            // 
+            // btngiris
+            // 
+            this.btngiris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(193)))), ((int)(((byte)(201)))));
+            this.btngiris.FlatAppearance.BorderSize = 0;
+            this.btngiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btngiris.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngiris.ForeColor = System.Drawing.Color.White;
+            this.btngiris.Location = new System.Drawing.Point(12, 374);
+            this.btngiris.Name = "btngiris";
+            this.btngiris.Size = new System.Drawing.Size(145, 47);
+            this.btngiris.TabIndex = 5;
+            this.btngiris.Text = "Giriş yap";
+            this.btngiris.UseVisualStyleBackColor = false;
+            this.btngiris.Click += new System.EventHandler(this.btngiris_Click);
+            // 
+            // pnlsifre
+            // 
+            this.pnlsifre.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlsifre.Controls.Add(this.lblsifre);
+            this.pnlsifre.Controls.Add(this.txtsifre);
+            this.pnlsifre.Controls.Add(this.pictureBox3);
+            this.pnlsifre.Location = new System.Drawing.Point(0, 287);
+            this.pnlsifre.Name = "pnlsifre";
+            this.pnlsifre.Size = new System.Drawing.Size(700, 50);
+            this.pnlsifre.TabIndex = 4;
+            // 
+            // lblsifre
+            // 
+            this.lblsifre.AutoSize = true;
+            this.lblsifre.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(81)))), ((int)(((byte)(133)))));
+            this.lblsifre.Location = new System.Drawing.Point(670, 8);
+            this.lblsifre.Name = "lblsifre";
+            this.lblsifre.Size = new System.Drawing.Size(26, 34);
+            this.lblsifre.TabIndex = 7;
+            this.lblsifre.Text = "!";
+            this.lblsifre.Visible = false;
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtsifre.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.txtsifre.Location = new System.Drawing.Point(58, 16);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.Size = new System.Drawing.Size(606, 25);
+            this.txtsifre.TabIndex = 4;
+            this.txtsifre.UseSystemPasswordChar = true;
+            this.txtsifre.Click += new System.EventHandler(this.txtsifre_Click);
+            this.txtsifre.TextChanged += new System.EventHandler(this.txtsifre_TextChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 7);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseDown);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseUp);
+            // 
+            // pnlad
+            // 
+            this.pnlad.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlad.Controls.Add(this.lblad);
+            this.pnlad.Controls.Add(this.txtad);
+            this.pnlad.Controls.Add(this.pictureBox2);
+            this.pnlad.Location = new System.Drawing.Point(0, 211);
+            this.pnlad.Name = "pnlad";
+            this.pnlad.Size = new System.Drawing.Size(700, 50);
+            this.pnlad.TabIndex = 3;
+            // 
+            // lblad
+            // 
+            this.lblad.AutoSize = true;
+            this.lblad.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(81)))), ((int)(((byte)(133)))));
+            this.lblad.Location = new System.Drawing.Point(670, 11);
+            this.lblad.Name = "lblad";
+            this.lblad.Size = new System.Drawing.Size(26, 34);
+            this.lblad.TabIndex = 6;
+            this.lblad.Text = "!";
+            this.lblad.Visible = false;
+            // 
+            // txtad
+            // 
+            this.txtad.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtad.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.txtad.Location = new System.Drawing.Point(58, 14);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(606, 25);
+            this.txtad.TabIndex = 3;
+            this.txtad.Click += new System.EventHandler(this.txtad_Click);
+            this.txtad.TextChanged += new System.EventHandler(this.txtad_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(193)))), ((int)(((byte)(201)))));
+            this.label2.Location = new System.Drawing.Point(17, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hesabınızı giriniz";
+            // 
+            // btnkapat
+            // 
+            this.btnkapat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnkapat.FlatAppearance.BorderSize = 0;
+            this.btnkapat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnkapat.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnkapat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnkapat.Location = new System.Drawing.Point(660, 0);
+            this.btnkapat.Name = "btnkapat";
+            this.btnkapat.Size = new System.Drawing.Size(40, 40);
+            this.btnkapat.TabIndex = 0;
+            this.btnkapat.Text = "X";
+            this.btnkapat.UseVisualStyleBackColor = true;
+            this.btnkapat.Click += new System.EventHandler(this.btnkapat_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 550);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.pnlsifre.ResumeLayout(false);
+            this.pnlsifre.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.pnlad.ResumeLayout(false);
+            this.pnlad.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnkapat;
+        private System.Windows.Forms.Panel pnlsifre;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel pnlad;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btngiris;
+        private System.Windows.Forms.Label lblhata;
+        private System.Windows.Forms.Label lblsifre;
+        private System.Windows.Forms.Label lblad;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ HastaneSistemi/Login.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace HastaneSistemi
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=YAKUP\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True");
+        private void btnkapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtad_Click(object sender, EventArgs e)
+        {
+            txtad.BackColor = Color.White;
+            pnlad.BackColor = Color.White;
+            txtsifre.BackColor = SystemColors.Control;
+            pnlsifre.BackColor = SystemColors.Control;
+            lblad.Visible = false;
+
+            if (txtsifre.Text=="")
+            {
+                lblsifre.Visible = true;
+                lblhata.Text = "Formu doldurunuz!!";
+            }
+
+        }
+
+        private void txtsifre_Click(object sender, EventArgs e)
+        {
+            txtsifre.BackColor = Color.White;
+            pnlsifre.BackColor = Color.White;
+            txtad.BackColor = SystemColors.Control;
+            pnlad.BackColor = SystemColors.Control;
+            lblsifre.Visible = false;
+
+            if (txtad.Text == "")
+            {
+                lblad.Visible = true;
+                lblhata.Text = "Formu doldurunuz!!";
+
+            }
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtsifre.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtsifre.UseSystemPasswordChar = true;
+        }
+
+        private void txtad_TextChanged(object sender, EventArgs e)
+        {
+            if (txtad.Text == "")
+            {
+                lblad.Visible = true;
+                lblhata.Text = "Formu doldurunuz!!";
+            }
+            else
+            {
+                lblad.Visible = false;
+                lblhata.Text = "";
+            }
+        }
+
+        private void txtsifre_TextChanged(object sender, EventArgs e)
+        {
+            if (txtsifre.Text == "")
+            {
+                lblsifre.Visible = true;
+                lblhata.Text = "Formu doldurunuz!!";
+            }
+            else
+            {
+                lblsifre.Visible = false;
+                lblhata.Text = "";
+            }
+        }
+
+        private void btngiris_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            try
+            {
+                
+                SqlCommand komut = new SqlCommand("select * from Users inner join roles on users.role = roles.role where username = @p1 and password = @p2", con);
+                komut.Parameters.AddWithValue("@p1", txtad.Text);
+                komut.Parameters.AddWithValue("@p2", txtsifre.Text);
+                SqlDataAdapter sda = new SqlDataAdapter(komut);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    string rol = dt.Rows[0][5].ToString();
+                    if (rol == "doktor")
+                    {
+                        MessageBox.Show("doktor");
+                    }
+                    else if (rol == "sekreter")
+                    {
+                        MessageBox.Show("sekreter");
+
+                    }
+                }
+                else
+                {
+                    lblhata.Text = "Hatalı kullancı adı veya sifre !!";
+                }
+
+                con.Close();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                con.Close();
+            }
+        }
+    }
+}
